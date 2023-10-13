db7a085d18c37ad89ee1c3f101772d7f525270ef
Ksu <ksysha.2002r@gmail.com>
2023-10-14 01:18:55 +0300

    fix db

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     SECRET_KEY = 'hard to guess string'
     SQLALCHEMY_DATABASE_URI = \
-        "postgresql+asyncpg://ksyu7650:04042002Mm!@77.222.36.9:18003/ksyu7650"
+        "postgresql://ksyu7650:04042002Mm!@77.222.36.9:18003/ksyu7650"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "abrleva08"
     JWT_REFRESH_SECRET_KEY = "refresh_abrleva08"
